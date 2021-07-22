2bea9050efabb903086f5c9617d6ef1dceb636c0
Naveigell <yosua.yerikho123@gmail.com>
2021-07-22 20:33:47 +0800

    Update sql database

--- pengarsipan_surat_masuk_dan_keluar.sql
+++ pengarsipan_surat_masuk_dan_keluar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2021 at 02:19 PM
+-- Generation Time: Jul 22, 2021 at 02:33 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.13
 
@@ -96,32 +96,8 @@ CREATE TABLE `tb_suratmasuk` (
 --
 
 INSERT INTO `tb_suratmasuk` (`id_sm`, `no_surat`, `perihal`, `pengirim_id`, `penerima_id`, `no_hp`, `tanggal_sm`, `dibaca`, `lampiran`) VALUES
-(30, 'SM/SM/8/2/2/2', 'Permohonan depopulasi', 1, 7, '08192378124', '2021-07-14', 0, '60f909d4e1ef320210807012256.pdf'),
-(31, 'SK/SK/10/2021', 'Permohonan depopulasi', 7, 7, '0985552231314', '2021-07-09', 1, '60f9618ce1fe620210722141612.pdf'),
-(32, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '0985552231314', '2021-07-17', 1, '60f90baf9b71320210807092251.pdf'),
-(33, 'SM/SM/8/2/2/2', 'Permohonan depopulasi', 7, 1, '08192378124', '2021-07-01', 1, '60f911d69381f20210807362206.pdf'),
-(34, 'SM/SM/8/2/2/2', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-08', 0, '60f9134ce6ce820210807422220.pdf'),
-(35, 'SM/SM/1/1999', 'perihal adalah sebuah perihal', 7, 1, '08192378124', '2021-07-23', 1, '60f913ca7e90a20210807442226.pdf'),
-(36, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-19', 1, '60f914179714920210807452243.pdf'),
-(37, 'SM/SM/8/2/2/2', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-10', 0, '60f914368ee3420210807462214.pdf'),
-(38, 'SM/SM/8/2/2/2', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-10', 0, '60f91478441e520210807472220.pdf'),
-(39, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 1, '60f91592b6f2520210807522202.pdf'),
-(40, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 1, '60f915b24193b20210807522234.pdf'),
-(41, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f915d17010920210807532205.pdf'),
-(42, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f9161e8eccd20210807542222.pdf'),
-(43, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f9171fe86de20210807582239.pdf'),
-(44, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f917695e37c20210807592253.pdf'),
-(45, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f918130aecf20210907022243.pdf'),
-(46, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f918410399620210907032229.pdf'),
-(47, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f918d95cf9e20210907062201.pdf'),
-(48, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f9194b8678520210907072255.pdf'),
-(49, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f919819648a20210907082249.pdf'),
-(50, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f919b2d43fc20210907092238.pdf'),
-(51, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f91b08b01ce20210907152220.pdf'),
-(52, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f91b7a39e9920210907172214.pdf'),
-(53, 'SM/SM/1/1999', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-16', 0, '60f91bcba407b20210907182235.pdf'),
-(54, 'SM/SM/8/2/2/2', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-09', 0, '60f95cebca18820211307562227.pdf'),
-(55, 'SM/SM/8/2/2/2', 'Permohonan depopulasi', 7, 1, '08192374154', '2021-07-09', 0, '60f95d169d9f320211307572210.pdf');
+(56, 'SK/SK/110/1999', 'Permohonan depopulasi', 1, 3, '08192374154', '2021-07-16', 0, '60f963d44e2cc20211407252256.pdf'),
+(57, 'SK/SK/110/1999', 'Permohonan depopulasi', 1, 7, '08192378124', '2021-07-02', 0, '60f963ea073cd20211407262218.pdf');
 
 -- --------------------------------------------------------
 
@@ -195,7 +171,7 @@ ALTER TABLE `tb_suratkeluar`
 -- AUTO_INCREMENT for table `tb_suratmasuk`
 --
 ALTER TABLE `tb_suratmasuk`
-  MODIFY `id_sm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id_sm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT for table `telegram_users`
